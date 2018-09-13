9f11b469e0fc755771063a818fc10d11906265ff
ViniciusTomeVieira <vinny.vieira99@hotmail.com>
2018-09-13 02:15:39 -0300

    Commit new Batalha

--- src/SuperGole/control/IniciarJogo.java
+++ src/SuperGole/control/IniciarJogo.java
@@ -44,6 +44,17 @@ public class IniciarJogo {
             maoPc.add(deckPc.dequeue());
         }
     }
+     public static int batalha(Bebidas cartaPC, Bebidas cartaUsu, int atributo){
+         switch(atributo){
+             case 1:  if(cartaPC.getPreco() > cartaUsu.getPreco()){return -1;}else{return 1;}
+             case 2:  if(cartaPC.getGosto() > cartaUsu.getGosto()){return 1;}else{return -1;}
+             case 3:  if(cartaPC.getTeorAlco()> cartaUsu.getTeorAlco()){return 1;}else{return -1;}
+             case 4:  if(cartaPC.getAmnesia()> cartaUsu.getAmnesia()){return 1;}else{return -1;}
+             case 5:  if(cartaPC.getCustBene()> cartaUsu.getCustBene()){return 1;}else{return -1;}
+
+             default: return 0;
+         }
+     }
     
     public static void main(String [] args){
         MecanicaPC pc = new MecanicaPC();
@@ -69,6 +80,26 @@ public class IniciarJogo {
         System.out.println("---------------------");
         System.out.println("Atributo:  " + atributo);
 
+    }
+
+    public static ArrayList<Bebidas> getMaoPc() {
+        return maoPc;
+    }
+
+    public static ArrayList<Bebidas> getMaoUsuario() {
+        return maoUsuario;
+    }
+
+    public static ArrayList<Bebidas> getDeck() {
+        return deck;
+    }
+
+    public static LinkedQueue<Bebidas> getDeckPc() {
+        return deckPc;
+    }
+
+    public static LinkedQueue<Bebidas> getDeckUsuario() {
+        return deckUsuario;
     }
     
     
